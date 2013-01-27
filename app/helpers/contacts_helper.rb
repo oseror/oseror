6668c09dbb85ffa9190cc29733c77550d2d8f1fc
Sanjib <sanjib@oseror.com>
2013-01-28 01:09:47 +0530

    Changes on contact page UI.

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,12 +1,6 @@
 module ContactsHelper
   def service
-    services = [
-      ["Web Developement",],
-      ["Mobility",],
-      ["Outsourcing",],
-      ["Test Enginnering",],
-      ['Design']
-    ]
+    %w(Web Developement Mobility Outsourcing Test Enginnering Design)
   end
 
 end
